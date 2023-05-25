2f100578f5ccb81340a41bc4ad40027a9d7ec283
Thomas Dhaussy <94963203+DHS5@users.noreply.github.com>
2023-05-25 10:05:56 +0200

    Update SceneManager.cs

--- Assets/Utility/Scene Creation System/SceneManager.cs
+++ Assets/Utility/Scene Creation System/SceneManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Scripting;
 
 namespace Dhs5.Utility.SceneCreation
 {
@@ -12,5 +13,28 @@ namespace Dhs5.Utility.SceneCreation
         {
             SceneState.SetSceneVars(sceneVariablesSO);
         }
+
+        #region SceneClock Actions
+        [Preserve]
+        public void StartTimeline(string timelineID)
+        {
+            SceneClock.Instance.StartTimeline(timelineID);
+        }
+        [Preserve]
+        public void StartTimeline(string timelineID, int step)
+        {
+            SceneClock.Instance.StartTimeline(timelineID, step);
+        }
+        [Preserve]
+        public void StopTimeline(string timelineID)
+        {
+            SceneClock.Instance.StopTimeline(timelineID);
+        }
+        [Preserve]
+        public void TimelineGoToStep(string timelineID, int step, bool interrupt)
+        {
+            SceneClock.Instance.GoToStep(timelineID, step, interrupt);
+        }
+        #endregion
     }
 }
